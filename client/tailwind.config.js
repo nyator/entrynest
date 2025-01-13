5f03b093fe9055e7f07779b4892391796c0842ec
Henry <hhen5562@gmail.com>
2025-01-13 02:32:50 +0000

    `Update tailwind.config.js with custom screen sizes`

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "520px",
+      md: "890px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    
     fontFamily: {
       sora: ["Sora", "serif"]
 
