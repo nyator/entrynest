9aa8c6229963dfc82cb25089b15cb237e667dcde
Henry <hhen5562@gmail.com>
2025-02-06 02:18:19 +0000

    `Added cookie parser middleware and implemented checkAuth route`

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -9,9 +10,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000; // But PORT 5000 is already taken by macos
 
-app.use(express.json());
+app.use(express.json()); //  allows to parse incoming request:req:body
+app.use(cookieParser()); // allows to parse incoming cookies
 
-app.use("/api/auth", authRoutes); // Allows you to parse incoming requests from user
+app.use("/api/auth", authRoutes); // allows  to parse incoming requests from user
 
 app.listen(PORT, () => {
   connectDB();
