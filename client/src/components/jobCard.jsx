b80425a5f704af9df62e8a88b429909cc9db3365
Henry <hhen5562@gmail.com>
2025-03-19 18:06:58 +0000

    Enhance JobCard component by adding optional props for company, location, salary, and position, improving customization and default values

--- client/src/components/jobCard.jsx
+++ client/src/components/jobCard.jsx
@@ -10,6 +10,7 @@ import { RiCoinsFill } from "react-icons/ri";
 const JobCard = ({
   type,
   timePosted,
+  company,
   postedBy,
   avatar,
   location,
@@ -25,13 +26,17 @@ const JobCard = ({
     >
       <div className="space-y-4 rounded-[25px] bg-red-200 p-4 ">
         <div className="flex justify-between items-center">
-          <div className={`${cardBStyle}`}>Internship</div>
-          <div className={`${cardBStyle}`}>Posted one month ago</div>
+          <div className={`${cardBStyle}`}>{type || "Internship"}</div>
+          <div className={`${cardBStyle}`}>
+            {timePosted || "Posted one month ago"}
+          </div>
         </div>
         <div className="flex justify-between items-center">
-          <h1 className="font-SatoshiMedium text-lg">Company's Name</h1>
+          <h1 className="font-SatoshiMedium text-lg">
+            {company || "Company's Name"}
+          </h1>
           <img
-            src={fr}
+            src={avatar || fr}
             alt="avatar"
             className="rounded-full w-[43px] h-[43px] ring-2 ring-black/20 "
           />
@@ -39,28 +44,30 @@ const JobCard = ({
         <div className="flex justify-between gap-4 items-center">
           <p className="flex items-center gap-1 text-black/40">
             <HiLocationMarker />
-            Greater Accra Region
+            {location || "Greater Accra Region"}
           </p>
           <p className="flex items-center gap-1 text-black/40">
             <PiBuildingOfficeFill />
-            On-site
+            {style || "On-site"}
           </p>
           <p className="flex items-center gap-1 text-black/40">
             <RiCoinsFill />
-            confidential
+            {salary || "confidential"}
           </p>
         </div>
         <div>
-          <h1 className="font-SatoshiMedium text-lg">Position</h1>
+          <h1 className="font-SatoshiMedium text-lg">
+            {position || "Position"}
+          </h1>
         </div>
         <div className="w-full bg-black/40 h-[1.5px]"></div>
         <div>
-          <p className={`${cardTag}`}>Sales & Marketing</p>
+          <p className={`${cardTag}`}>{tag || "Sales & Marketing"}</p>
         </div>
       </div>
       <div className="text-center weight-400 space-y-4">
         <h1>
-          posted by <span>entrynest 3rd party</span>
+          posted by <span>{postedBy || "entrynest 3rd party"}</span>
         </h1>
         <button className={`${cardButton} rounded-full`} onClick={onClick}>
           View Details
