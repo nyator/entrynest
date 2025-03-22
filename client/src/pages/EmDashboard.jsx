df328a2cc3452a2b5e04ccb6953c972490c3f4da
Henry <hhen5562@gmail.com>
2025-03-22 00:23:17 +0000

    Refactor EmDashboard component by removing unused imports and improving code formatting

--- client/src/pages/EmDashboard.jsx
+++ client/src/pages/EmDashboard.jsx
@@ -3,9 +3,6 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { input, button } from "../constants/styles";
 import { Link } from "react-router";
-// import { Button } from '@headlessui/react'
-
-
 
 const EmDashboard = () => {
 
@@ -13,7 +10,7 @@ const EmDashboard = () => {
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Employer Dashboard</h1>
       <Link to="/create-job" className="text-white bg-primary rounded-xl p-2 hover:bg-primary-dark">Post a Job</Link>
-    </div>
+   </div>
   );
 };
 
