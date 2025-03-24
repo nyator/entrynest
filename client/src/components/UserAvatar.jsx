8c3d2864acbd6d03e48647096f1e85f2471c0e0a
Henry <hhen5562@gmail.com>
2025-03-24 15:36:54 +0000

    Refactor UserAvatar component for improved code consistency and styling; update logout button styles and toast messages

--- client/src/components/UserAvatar.jsx
+++ client/src/components/UserAvatar.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuthStore } from '../store/authStore';
-import { toast } from 'react-toastify';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
+import { toast } from "react-toastify";
 
-import { hover } from '../constants/styles';
+import { hover } from "../constants/styles";
 
 const UserAvatar = () => {
-
   const [isOpen, setIsOpen] = useState(false);
   const { user, logout } = useAuthStore();
 
@@ -15,14 +14,13 @@ const UserAvatar = () => {
   const handleLogout = async () => {
     try {
       await logout();
-      navigate('/');
+      navigate("/");
       useAuthStore.setState({ isCheckingAuth: false });
       useAuthStore.setState({ isAuthenticated: false });
       useAuthStore.setState({ user: null });
-      toast.success('Logout successful');
-
+      toast.success("Logout successful");
     } catch (error) {
-      console.error('Logout failed:', error);
+      console.error("Logout failed:", error);
     }
   };
 
@@ -34,17 +32,25 @@ const UserAvatar = () => {
         onClick={toggleDropdown}
         className="flex items-center gap-2 focus:outline-none"
       >
-        <div className={`w-10 h-10 rounded-full bg-primary flex items-center justify-center text-white ${hover}`}>
-          {[user?.firstname?.charAt(0), user?.lastname?.charAt(0)] || 'U'}
+        <div
+          className={`w-10 h-10 rounded-full bg-primary flex items-center justify-center text-white ${hover}`}
+        >
+          {[user?.firstname?.charAt(0), user?.lastname?.charAt(0)] || "U"}
         </div>
       </button>
 
       {isOpen && (
         <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-          <div className="py-1">
+          <div className="">
             <button
               onClick={handleLogout}
-              className="block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              className="block rounded-t-md w-full px-4 py-4 text-sm text-gray-700 hover:bg-primary/20 transform-all ease-in-out duration-300 border-b border-gray/40"
+            >
+              Profile
+            </button>
+            <button
+              onClick={handleLogout}
+              className="block rounded-b-md w-full px-4 py-4 text-sm text-gray-700 hover:bg-primary/20 transform-all ease-in-out duration-300"
             >
               Logout
             </button>
